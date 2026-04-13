7d1c8fe275175ad85016ad0fb9843f39a6a96e34
Maksenoff <115073041+Maksenoff@users.noreply.github.com>
2026-04-13 14:20:50 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ echo "→ Waiting for database to be ready..."
 sleep 5
 
 echo "→ Running database migrations..."
-#php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration || true
+php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration || true
 
 echo "→ Warming up Symfony cache..."
 php bin/console cache:warmup --no-interaction 2>/dev/null || true
